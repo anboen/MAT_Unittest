5ae6c1384fc8feb4fa02f3f0199f97ba4810d0e0
andreboni <andreboni@192.168.0.16>
2022-10-18 07:50:55 +0200

    new functionbased tests

---
+++ tests/BMITest.m
@@ -0,0 +1,31 @@
+
+function tests = BMITest
+    tests = functiontests(localfunctions);
+end
+
+%% Test BMI good
+function goodCaseTest(testCase)
+    height = 1.88;
+    weight = 82;
+    exp_bmi = 23.2;
+
+    act_bmi = calcBMI(height, weight);
+    verifyEqual(testCase,act_bmi,exp_bmi)
+end
+
+%% Test BMI error
+function errorThrowTest(testCase)
+    height = 0;
+    weight = 82;
+    try 
+        calcBMI(height, weight);
+        verifyFail(testCase)
+    catch
+        % what we expect
+    end
+
+end
+
+ 
+    
+
